acb8c5b36a8f3e7fdda6022778bca90edf552d56
berkay <berkay@berkay-MacBook-Air.local>
2025-10-19 21:32:49 +0300

    19.10.2025

---
+++ test_6.py
@@ -0,0 +1,22 @@
+#n = 0
+#where = input("Go left or right?")
+#while where == "right":
+#    n = n+1
+ #   if n > 2:
+ #       print(":(")
+ #   where = input("Go left or right?")
+#print("You got out.")
+
+#while True:
+#    print("noooooo")
+
+#for i in range(1,4,1):
+#    print(i)
+
+sum = 0
+start = 3
+end = 6
+for i in range(start,end):
+    sum += i
+
+print(sum)    
