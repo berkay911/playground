11aa1c6ab24231813f68ae3c7b44915953783335
berkay <berkay@berkay-MacBook-Air.local>
2025-10-20 15:20:51 +0300

    20.10.2025

---
+++ l3.py
@@ -0,0 +1,62 @@
+#while True:
+ #   print("Selamsssss")
+  #  break
+#print("Naber")
+
+#where = input("Koridorda sıkıştın bro sağ mı sol mu?")
+#while where == "sağ":
+ #   print("Bir canavar seni yuttu")
+ #   where = input("Koridorda sıkıştın bro sağ mı sol mu?")
+
+#print("Tebrikler canavardan kaçtın")
+
+#n = 0
+#while n <= 10:
+#    print(n)
+#    n = n + 1
+#    if n == 10:
+#        print("Sonuç 10 oldu, döngüden çıkılıyor.")
+#        break
+
+
+#for i in range(0,20,2):
+#    print(i)   
+# 
+
+#for i in range(20):
+ #   if i % 2 == 0:
+ #       print(i)
+
+#n = -1
+#while n <= 10:
+#    n = n + 1
+#    if n % 2 == 0:
+ #       print(n)
+
+
+#first_name = "Ali"
+#last_name = "Dayı"
+
+#full_name = first_name + " " + last_name
+
+#for letter in full_name:
+#    print(letter)
+
+
+#first_name = "Ali"
+#last_name = "Dayı"
+#
+#full_name = first_name + " " + last_name
+#print(full_name)
+
+#for i in range(len(full_name)):
+#    if full_name[i] == "i":
+#     break   
+#    print(full_name[i])
+
+result = 1
+for x in range(1,6):
+   result *= x
+print(result)
+
+
