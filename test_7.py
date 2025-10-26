71485e18c6294bfa877eda8b72d195e915ebdafe
Berkay <berkay9114@proton.me>
2025-10-26 21:15:43 +0300

    26.10.2025

---
+++ test_7.py
@@ -0,0 +1,28 @@
+#number_of_evens = 0 
+
+#for i in range(10):
+#    if i % 2 == 0:
+#        number_of_evens += 1
+
+#print("Numbers of even numbers:", number_of_evens) 
+
+#s = "abhca"
+#seen = ""
+
+#for char in s:
+#  # if char not in seen:
+   #     seen += char
+#print(seen)
+#print("Unique characters:", len(seen))
+
+secret_number = 4
+
+for i in range (1,11):
+    if i == secret_number:
+        print("You found it!", i)
+        break
+    else:
+        print("Not this time")
+        break
+
+
